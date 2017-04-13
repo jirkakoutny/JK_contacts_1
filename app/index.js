318dd58acf56baa56f560e1e670ed5a813943356
jirkakoutny <koutny.jiri@gmail.com>
2017-04-13 14:02:33 +0200

    Merged with Redux example
    https://medium.com/differential/handling-offline-actions-in-react-native
    -74949cbfabf2

--- app/index.js
+++ app/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import { Platform } from 'react-native';
+// import React from 'react';
+// import { Platform } from 'react-native';
 
-import { Tabs, Drawer } from './config/router.js';
+// import { Tabs, Drawer } from './config/router.js';
 
-const App = () => {
-  if (Platform.OS === 'ios') {
-    return <Tabs />
+// const App = () => {
+//   if (Platform.OS === 'ios') {
+//     return <Tabs />
+//   }
+
+//   return <Drawer />;
+// };
+
+// export default App;
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import NameList from './NameList';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      store: configureStore(() => this.setState({ isLoading: false })),
+    };
   }
 
-  return <Drawer />;
-};
+  render() {
+   if (this.state.isLoading) return null;
+   return (
+     <Provider store={this.state.store}>
+       <NameList />
+     </Provider>
+   );
+  }
+}
 
 export default App;
