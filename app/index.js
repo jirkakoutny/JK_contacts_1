189f47f4edaedfde0ff5cd9148b33acf9b2e8e1a
JirKa <koutny.jiri@gmail.com>
2017-05-22 13:26:13 +0200

    Navigation in my store

--- app/index.js
+++ app/index.js
@@ -2,34 +2,34 @@ import React, { Component } from 'react';
 import { AppRegistry, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-// import configureStore from './redux/store';
+import configureStore from './redux/store';
 
-import AppReducer from './redux/reducer';
+// import AppReducer from './redux/reducer';
 import AppWithNavigationState from './AppNavigator';
 
 // Delete
 import { Tabs, Drawer } from './config/router.js';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     isLoading: true,
-  //     store: configureStore(() => this.setState({ isLoading: false })),
-  //   };    
-  //   console.log("******************** App constructor ******************** ");
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      store: configureStore(() => this.setState({ isLoading: false })),
+    };    
+    console.log("******************** App constructor ******************** ");
+  }
 
-  store = createStore(AppReducer);
+  // store = createStore(AppReducer);
   
   render() {
-    // if (this.state.isLoading)
-    //   return null;
+    if (this.state.isLoading)
+      return null;
 
     // var index = Platform.OS === 'ios' ? <Tabs /> : <Drawer />
 
     return (
-      <Provider store={this.store}>
+      <Provider store={this.state.store}>
         <AppWithNavigationState />
       </Provider>
       //<Provider store={this.state.store}>
